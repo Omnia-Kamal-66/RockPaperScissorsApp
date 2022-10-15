5d944270b9fba4e86586a306eb4f0bf828b08132
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2022-10-15 16:47:29 +0200

    game is done

--- app.js
+++ app.js
@@ -1,1 +1,121 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = "ROCK";
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER WINS";
+
+let gameIsRunning = false;
+const getPlayerChoice = () => {
+  const selection = prompt("Rock, Paper or Scissors", "").toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && Selection !== SCISSORS) {
+    alert(`Invalid choice!, we choose ${DEFAULT_USER_CHOICE} for you`);
+    return;
+  }
+
+  return selection;
+};
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) => {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
+startGameBtn.addEventListener("click", () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is starting!");
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  let winner;
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice);
+  } else {
+    winner = getWinner(computerChoice);
+  }
+  let message = `you picked ${
+    playerChoice || DEFAULT_USER_CHOICE
+  }, computer picked ${computerChoice}, therefor you`;
+
+  if (winner === RESULT_DRAW) {
+    message = message + "had a draw";
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + "won";
+  } else {
+    message = message + "lost";
+  }
+  alert(message);
+  gameIsRunning = false;
+});
+
+// not related to the game
+const combine = (resultHandler, operation, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+  let sum = 0;
+  for (const num of numbers) {
+    if (operation === "ADD") {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
+  }
+  resultHandler(sum);
+};
+
+/* const subtractUp = (resultHandler, ...numbers) => {
+  let sum = 0;
+  for (const num of numbers) {
+    sum -= num;
+  }
+  resultHandler(sum);
+}; */
+
+const showResult = (messageText, result) => {
+  alert(messageText + "  " + result);
+};
+combine(
+  showResult.bind(this, "the result after adding all numbers is"),
+  "ADD",
+  1,
+  "ff",
+  3,
+  4,
+  5,
+  6,
+  7
+);
+combine(
+  showResult.bind(this, "the result after subtracting all numbers is"),
+  "SUBTRACT",
+  4,
+  5,
+  5,
+  5,
+  7,
+  9
+);
